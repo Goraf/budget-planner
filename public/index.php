f4377dfeb2c29b5b3a5131a1ffa3794c6acbc262
Goraf <22941576+Goraf@users.noreply.github.com>
2018-09-15 14:27:49 +0200

    Setup middleware dispatcher

--- public/index.php
+++ public/index.php
@@ -2,8 +2,10 @@
 
 declare(strict_types=1);
 
-use DI\ContainerBuilder;
 use BudgetPlanner\HomePageController;
+use DI\ContainerBuilder;
+use Relay\Relay;
+use Zend\Diactoros\ServerRequestFactory;
 use function DI\create;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
@@ -16,6 +18,11 @@ $containerBuilder->addDefinitions([
 ]);
 
 $container = $containerBuilder->build();
+
+$middlewareQueue = [];
+
+$requestHandler = new Relay($middlewareQueue);
+$requestHandler->handle(ServerRequestFactory::fromGlobals());
 
 $homePage = $container->get(HomePageController::class);
 $homePage->show();
