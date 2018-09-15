ba206cf5893d232abe3abd2082ee771d8565e11f
Goraf <22941576+Goraf@users.noreply.github.com>
2018-09-15 20:07:11 +0200

    Move app code out of index file

--- public/index.php
+++ public/index.php
@@ -2,37 +2,6 @@
 
 declare(strict_types=1);
 
-use BudgetPlanner\HomePageController;
-use DI\ContainerBuilder;
-use FastRoute\RouteCollector;
-use Middlewares\FastRoute;
-use Middlewares\RequestHandler;
-use Relay\Relay;
-use Zend\Diactoros\Response\SapiEmitter;
-use Zend\Diactoros\ServerRequestFactory;
-use function DI\create;
-use function FastRoute\simpleDispatcher;
+require dirname(__DIR__) . '/vendor/autoload.php';
 
-require_once dirname(__DIR__) . '/vendor/autoload.php';
-
-$containerBuilder = new ContainerBuilder();
-$containerBuilder->useAutowiring(false);
-$containerBuilder->useAnnotations(false);
-$containerBuilder->addDefinitions([
-    HomePageController::class => create(HomePageController::class)
-]);
-
-$container = $containerBuilder->build();
-
-$dispatcher = simpleDispatcher(function (RouteCollector $r) {
-
-});
-
-$middlewareQueue[] = new FastRoute($dispatcher);
-$middlewareQueue[] = new RequestHandler($container);
-
-$requestHandler = new Relay($middlewareQueue);
-$response = $requestHandler->handle(ServerRequestFactory::fromGlobals());
-
-$emiter = new SapiEmitter();
-$emiter->emit($response);
+require dirname(__DIR__) . '/src/bootstrap.php';
---
+++ src/bootstrap.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use BudgetPlanner\HomePageController;
+use DI\ContainerBuilder;
+use FastRoute\RouteCollector;
+use Middlewares\FastRoute;
+use Middlewares\RequestHandler;
+use Relay\Relay;
+use Zend\Diactoros\Response\SapiEmitter;
+use Zend\Diactoros\ServerRequestFactory;
+use function DI\create;
+use function FastRoute\simpleDispatcher;
+
+$containerBuilder = new ContainerBuilder();
+$containerBuilder->useAutowiring(false);
+$containerBuilder->useAnnotations(false);
+$containerBuilder->addDefinitions([
+    HomePageController::class => create(HomePageController::class)
+]);
+
+$container = $containerBuilder->build();
+
+$dispatcher = simpleDispatcher(function (RouteCollector $r) {
+
+});
+
+$middlewareQueue[] = new FastRoute($dispatcher);
+$middlewareQueue[] = new RequestHandler($container);
+
+$requestHandler = new Relay($middlewareQueue);
+$response = $requestHandler->handle(ServerRequestFactory::fromGlobals());
+
+$emiter = new SapiEmitter();
+$emiter->emit($response);
