4d0171d2c6d87d44ca5c28576fb1d3af4418c143
Goraf <22941576+Goraf@users.noreply.github.com>
2018-09-17 17:48:45 +0200

    Fix route definition for homepage

--- src/routes.php
+++ src/routes.php
@@ -4,5 +4,5 @@ declare(strict_types=1);
 
 return [
     // Routes definitions
-    ['GET', '/', ['BudgetPlanner\HomePageController', 'show']]
+    ['GET', '/', 'BudgetPlanner\HomePageController']
 ];
