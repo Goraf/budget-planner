32576b6e5237b73ec80eac60a385619b5a45e540
Goraf <22941576+Goraf@users.noreply.github.com>
2018-09-20 18:33:47 +0200

    Return 'index' view as a response to homepage request

--- src/HomePageController.php
+++ src/HomePageController.php
@@ -5,21 +5,27 @@ declare(strict_types=1);
 namespace BudgetPlanner;
 
 use Psr\Http\Message\ResponseInterface;
+use BudgetPlanner\Template\TemplateRendererInterface;
 
 final class HomePageController
 {
     /** @var ResponseInterface */
     private $response;
 
-    public function __construct(ResponseInterface $response)
+    /** @var TemplateRendererInterface */
+    private $templateRenderer;
+
+    public function __construct(ResponseInterface $response, TemplateRendererInterface $renderer)
     {
         $this->response = $response;
+        $this->templateRenderer = $renderer;
     }
 
     public function __invoke(): ResponseInterface
     {
+        $content = $this->templateRenderer->render('index.html');
         $response = $this->response->withHeader('Content-Type', 'text/html');
-        $response->getBody()->write('It works!');
+        $response->getBody()->write($content);
 
         return $response;
     }
