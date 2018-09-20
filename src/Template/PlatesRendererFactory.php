d07736e39d594431dee29a68e8db8b34ac921010
Goraf <22941576+Goraf@users.noreply.github.com>
2018-09-20 16:43:21 +0200

    Create factory for Plates renderer

---
+++ src/Template/PlatesRendererFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BudgetPlanner\Template;
+
+use Psr\Container\ContainerInterface;
+use League\Plates\Engine as PlatesEngine;
+
+final class PlatesRendererFactory
+{
+    public function __invoke(): PlatesRenderer
+    {
+        $engine = new PlatesEngine(__DIR__.'/../../resources/views');
+
+        $engine->setFileExtension(null);
+
+        return new PlatesRenderer($engine);
+    }
+}
