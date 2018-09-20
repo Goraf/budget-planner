a02823ee14ed8d5ef5be66eeb1578c0171859cd9
Goraf <22941576+Goraf@users.noreply.github.com>
2018-09-20 16:59:25 +0200

    Add Plates to container as a renderer

--- src/dependencies.php
+++ src/dependencies.php
@@ -3,10 +3,12 @@
 declare(strict_types=1);
 
 use BudgetPlanner\HomePageController;
+use BudgetPlanner\Template\PlatesRendererFactory;
 use DI\ContainerBuilder;
 use Psr\Http\Message\ResponseInterface;
 use Zend\Diactoros\Response;
 use function DI\create;
+use function DI\factory;
 use function DI\get;
 
 $containerBuilder = new ContainerBuilder();
@@ -14,11 +16,15 @@ $containerBuilder->useAutowiring(false);
 $containerBuilder->useAnnotations(false);
 
 $containerBuilder->addDefinitions([
-    HomePageController::class => create()->constructor(get('Response')),
+    HomePageController::class => create()->constructor(get('Response'), get('Renderer')),
     ResponseInterface::class => get('Response'),
+    TemplateRendererInterface::class => get('Renderer'),
+    'Renderer' => factory(PlatesRendererFactory::class),
     'Response' => function () {
         return new Response();
     },
+    // Factories
+    PlatesRendererFactory::class => create(),
 ]);
 
 $container = $containerBuilder->build();
