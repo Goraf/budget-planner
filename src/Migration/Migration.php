0a0ccc8e5aa1af2a7bfacb55445c780c7fcaa1ad
Goraf <22941576+Goraf@users.noreply.github.com>
2018-09-24 17:48:03 +0200

    Add base migration class

---
+++ src/Migration/Migration.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BudgetPlanner\Migration;
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Phinx\Migration\AbstractMigration;
+
+class Migration extends AbstractMigration
+{
+    /** @var \Illuminate\Database\Capsule\Manager $capsule */
+    protected $capsule;
+    /** @var \Illuminate\Database\Schema\Builder $scheme */
+    protected $scheme;
+
+    public function init() {
+        $this->capsule = new Capsule;
+        $this->capsule->addConnection([
+            'driver' => env('DB_DRIVER'),
+            'host' => env('DB_HOST'),
+            'port' => env('DB_PORT', ''),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            'charset' => env('DB_CHARSET'),
+            'collation' => env('DB_COLLATION'),
+            'prefix' => env('DB_PREFIX', ''),
+        ]);
+        $this->capsule->setAsGlobal();
+        $this->capsule->bootEloquent();
+        $this->scheme = $this->capsule->schema();
+    }
+}
