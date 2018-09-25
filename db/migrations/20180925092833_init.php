36d02f3955650cbcc225f54f2d3c8ec113196aef
Goraf <22941576+Goraf@users.noreply.github.com>
2018-09-25 12:33:01 +0200

    Add initial migration with users table

---
+++ db/migrations/20180925092833_init.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use BudgetPlanner\Migration\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class Init extends Migration
+{
+    public function up()
+    {
+        $this->scheme->create('users', function (Blueprint $table) {
+            $table->unsignedInteger('id')->autoIncrement();
+
+            $table->string('user_name', 30);
+
+            $table->string('email_adress', 40)->unique();
+
+            $table->string('password_salt', 60);
+
+            $table->string('password_hash', 60);
+        });
+    }
+
+    public function down()
+    {
+        $this->scheme->drop('users');
+    }
+}
