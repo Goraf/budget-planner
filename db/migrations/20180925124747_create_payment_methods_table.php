7c13796c14402c9a4342a5ec6ba54c766d8f8508
Goraf <22941576+Goraf@users.noreply.github.com>
2018-09-25 14:54:15 +0200

    Migration: create payment_methods table

---
+++ db/migrations/20180925124747_create_payment_methods_table.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use BudgetPlanner\Migration\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreatePaymentMethodsTable extends Migration
+{
+    public function up()
+    {
+        $this->scheme->create('payment_methods', function (Blueprint $table) {
+            $table->unsignedInteger('id')->autoIncrement();
+
+            $table->string('payment_method_name', 60);
+        });
+    }
+
+    public function down()
+    {
+        $this->scheme->drop('payment_methods');
+    }
+}
