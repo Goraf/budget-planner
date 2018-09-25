09a963642d1b3db90d9cca0f2ef0daaadde6229f
Goraf <22941576+Goraf@users.noreply.github.com>
2018-09-25 15:39:53 +0200

    Migration: create incomes table

---
+++ db/migrations/20180925133331_create_incomes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+use BudgetPlanner\Migration\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateIncomesTable extends Migration
+{
+    public function up()
+    {
+        $this->scheme->create('incomes', function (Blueprint $table) {
+            $table->unsignedInteger('id')->autoIncrement();
+
+            $table->unsignedInteger('account_id');
+            $table->foreign('account_id', 'incomes__account_id__fk')
+                ->references('id')->on('accounts');
+
+            $table->unsignedInteger('income_category_id');
+            $table->foreign('income_category_id', 'incomes__income_category_id__fk')
+                ->references('id')->on('income_categories');
+
+            $table->date('income_date');
+
+            $table->decimal('amount', 10, 2);
+
+            $table->string('income_comment', 190);
+        });
+    }
+
+    public function down()
+    {
+        $this->scheme->table('incomes', function (Blueprint $table) {
+            $table->dropForeign('incomes__account_id__fk');
+            $table->dropForeign('incomes__income_category_id__fk');
+        });
+        $this->scheme->drop('incomes');
+    }
+}
