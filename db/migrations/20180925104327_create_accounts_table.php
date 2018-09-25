d54dacd7ab8b91fe88a1b1c9856de64d168f81ae
Goraf <22941576+Goraf@users.noreply.github.com>
2018-09-25 13:09:43 +0200

    Migration: create accounts table

---
+++ db/migrations/20180925104327_create_accounts_table.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use BudgetPlanner\Migration\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateAccountsTable extends Migration
+{
+    public function up()
+    {
+        $this->scheme->create('accounts', function (Blueprint $table) {
+            $table->unsignedInteger('id')->autoIncrement();
+
+            $table->string('account_name', 40)->default('Account_name');
+        });
+    }
+
+    public function down()
+    {
+        $this->scheme->drop('accounts');
+    }
+}
